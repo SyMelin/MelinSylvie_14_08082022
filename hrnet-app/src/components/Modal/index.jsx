6f6e03ee4c319f1bf36904100c96066ad089fdc7
Sylvie Melin <sy.melin@gmail.com>
2022-08-19 09:54:15 +0200

    modal: handleKeyPress() added

--- hrnet-app/src/components/Modal/index.jsx
+++ hrnet-app/src/components/Modal/index.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 import { setModalState } from '../../utils/features/modal'
 import { setFormError } from '../../utils/features/createEmployeeForm'
@@ -5,6 +6,20 @@ import './Modal.css'
 
 function Modal({ id, children }) {
     const dispatch = useDispatch()
+
+    const handleKeyPress = (e) => {
+        if (e.key === "Escape") {
+            dispatch(setModalState())
+        }
+    }
+
+    useEffect(() => {
+        window.addEventListener("keydown", handleKeyPress);
+    
+        return () => {
+          window.removeEventListener("keydown", handleKeyPress);
+        }
+      }, [])
     
     return (
         <div id={id} className='modal'>
