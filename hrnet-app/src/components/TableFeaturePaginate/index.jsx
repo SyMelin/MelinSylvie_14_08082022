82bd92f67df82a5c411d20af5405461a677c8d6d
Sylvie Melin <sy.melin@gmail.com>
2022-09-07 20:34:28 +0200

    key added on paginate__button

--- hrnet-app/src/components/TableFeaturePaginate/index.jsx
+++ hrnet-app/src/components/TableFeaturePaginate/index.jsx
@@ -12,10 +12,10 @@ function TableFeaturePaginate() {
     return (
         <div className="table-features__paginate" id="employee-table__paginate">
             <button
+                key='button-paginate-previous'
                 type='button'
                 className="paginate__button previous disabled"
                 aria-controls="employee-table"
-                //  data-dt-idx="0"
                 tabIndex="-1"
                 id="employee-table__previous"
                 onClick={() => {
@@ -30,10 +30,10 @@ function TableFeaturePaginate() {
             <span>
                 {[...Array(employeeTable.nbOfPages)].map((e, index) => 
                     <button
+                        key={`button-paginate-number-${index + 1}`}
                         type="button"
                         className={ index === employeeTable.indexOfCurrentPage ? "paginate__button current" : "paginate__button"}
                         aria-controls="employee-table"
-                        // data-dt-idx={`${index + 1}`}
                         tabIndex="0"
                         onClick={() => {
                             dispatch(moveToPageIndex(index))
@@ -44,10 +44,10 @@ function TableFeaturePaginate() {
                 )}
             </span>
             <button
+                key='button-paginate-next'
                 type='button'
                 className="paginate__button next"
                 aria-controls="employee-table"
-                // data-dt-idx="3"
                 tabIndex="0"
                 id="employee-table__next"
                 onClick={() => {
