63f30bb5687eb5a1f61b796689f4e0bb13c3c2a7
Sylvie Melin <sy.melin@gmail.com>
2022-09-09 14:22:22 +0200

    first try of React test -  on ArrowUpSvg

---
+++ hrnet-app/src/components/ArrowUpSvg/ArrowUpSvg.test.js
@@ -0,0 +1,8 @@
+import ArrowUpSvg from './'
+import { render } from '@testing-library/react'
+
+describe('ArrowUpSvg', () => {
+    test('Should render without crashing', async () => {
+        render (<ArrowUpSvg />)
+    })
+})
