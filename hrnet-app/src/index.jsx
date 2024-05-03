63feb3530e7563b924cdd46ee7cf5182281cd26c
Sylvie Melin <sy.melin@gmail.com>
2024-05-03 18:58:01 +0200

    Migration to Vite

--- hrnet-app/src/index.jsx
+++ hrnet-app/src/index.jsx
@@ -5,8 +5,6 @@ import store from './utils/store'
 import App from './components/App'
 import './index.css'
 
-import reportWebVitals from './reportWebVitals'
-
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <Provider store={store}>
@@ -15,8 +13,3 @@ root.render(
     </React.StrictMode>
   </Provider>
 )
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
